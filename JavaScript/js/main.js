e9b698ab7599cef4dc69d00ef9dfb966deb27d4d
DeriuhinOleksandr <oleksandrderiuhin@gmail.com>
2024-10-07 20:30:16 +0300

    First commit in this Repo
    First commit in

---
+++ JavaScript/js/main.js
@@ -0,0 +1,30 @@
+//document.write("On JS");
+//<noscript>On JS !!!</noscript>
+//console.error(" Hi JS");
+
+//var num_1 = 5;
+//var num_2 = 15;
+
+//console.log(num);
+//console.log("Переменная :" + num_1);
+
+//var arr = [5,4,23,56,67,23];
+
+//console.log(arr[3])
+
+//for (var i = 1; i <= 195; i++){
+//  console.log(i);
+//}
+
+//var j = 10000;
+
+//while(j>=100){
+//  console.log(j);
+//  j-=100;
+//}
+
+var x = 100;
+
+do {
+ console.log(x);
+} while(x<98);
